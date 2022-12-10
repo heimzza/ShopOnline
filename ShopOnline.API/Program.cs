6c2805134a8cbfe4c5a55177b6374a8d6fd9e512
Hamza <hamzassm18@gmail.com>
2022-12-10 18:20:55 +0300

    [Feat]: Products page get products

--- ShopOnline.API/Program.cs
+++ ShopOnline.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using ShopOnline.API.Data;
 using ShopOnline.API.Repositories;
 using ShopOnline.API.Repositories.Contracts;
@@ -26,6 +27,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:7013", "https://localhost:7013")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+);
 
 app.UseHttpsRedirection();
 
